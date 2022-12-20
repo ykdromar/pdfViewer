5d71500767130a2aba389f18f6410d605e5efbcc
Yash <ykdromarpro@gmail.com>
2022-12-21 01:41:07 +0530

    rendered pdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 function App() {
-  return <div className="App">Getting Started</div>;
+  return (
+    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
+      <Document file="FCH.pdf" style={{ width: "100vw" }}>
+        <Page pageNumber={1} devicePixelRatio={7} scale={2} />
+      </Document>
+    </div>
+  );
 }
 
 export default App;
